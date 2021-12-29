3ee1aa4b17f8f300893ca2475109e887312bfdb8
ruslan_assimov <azimov_2001@bk.ru>
2021-12-29 18:14:38 +0300

    cicd

--- cicd-task/src/main/java/epam/lab/Constants.java
+++ cicd-task/src/main/java/epam/lab/Constants.java
@@ -1,6 +1,8 @@
 package epam.lab;
 
 public class Constants {
+    private Constants(){}
+    
     public static final String NO_MESSAGES = "no messages";
     public static final String PATH = "src\\messages.csv";
     public static final String FILE_NOT_FOUND = "file not found";
